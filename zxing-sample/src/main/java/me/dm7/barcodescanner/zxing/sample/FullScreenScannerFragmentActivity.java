d13f2e9a5f9939ee2b876697ef468c1948a4f2a3
Christian <chalea10@gmail.com>
2026-05-07 00:32:42 +0200

    Convert scanner activities to Kotlin

--- zxing-sample/src/main/java/me/dm7/barcodescanner/zxing/sample/FullScreenScannerFragmentActivity.java
+++ zxing-sample/src/main/java/me/dm7/barcodescanner/zxing/sample/FullScreenScannerFragmentActivity.java
@@ -1,18 +1,18 @@
-package me.dm7.barcodescanner.zxing.sample;
-
-
-import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import me.dm7.barcodescanner.zxing.sample.databinding.ActivityFullScreenScannerFragmentBinding;
-
-public class FullScreenScannerFragmentActivity extends AppCompatActivity {
-    @Override
-    public void onCreate(Bundle state) {
-        super.onCreate(state);
-
-        ActivityFullScreenScannerFragmentBinding binding = ActivityFullScreenScannerFragmentBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-    }
-}
+//package me.dm7.barcodescanner.zxing.sample;
+//
+//
+//import android.os.Bundle;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import me.dm7.barcodescanner.zxing.sample.databinding.ActivityFullScreenScannerFragmentBinding;
+//
+//public class FullScreenScannerFragmentActivity extends AppCompatActivity {
+//    @Override
+//    public void onCreate(Bundle state) {
+//        super.onCreate(state);
+//
+//        ActivityFullScreenScannerFragmentBinding binding = ActivityFullScreenScannerFragmentBinding.inflate(getLayoutInflater());
+//        setContentView(binding.getRoot());
+//    }
+//}
