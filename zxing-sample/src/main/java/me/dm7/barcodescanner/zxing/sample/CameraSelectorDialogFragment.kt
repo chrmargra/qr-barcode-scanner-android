454b51e1e137b663ba12ab4720054b0ec2d41ade
Christian <chalea10@gmail.com>
2026-05-07 00:26:24 +0200

    Convert scanner activity and fragments to Kotlin

---
+++ zxing-sample/src/main/java/me/dm7/barcodescanner/zxing/sample/CameraSelectorDialogFragment.kt
@@ -0,0 +1,73 @@
+package me.dm7.barcodescanner.zxing.sample
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.hardware.Camera
+import android.os.Bundle
+import androidx.fragment.app.DialogFragment
+
+class CameraSelectorDialogFragment : DialogFragment() {
+
+    interface CameraSelectorDialogListener {
+        fun onCameraSelected(cameraId: Int)
+    }
+
+    private var cameraId = -1
+    private var listener: CameraSelectorDialogListener? = null
+
+    override fun onCreate(state: Bundle?) {
+        super.onCreate(state)
+        retainInstance = true
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        val safeListener = listener
+        if (safeListener == null) {
+            dismiss()
+            return Dialog(requireContext())
+        }
+
+        val numberOfCameras = Camera.getNumberOfCameras()
+        val cameraNames = Array(numberOfCameras) { index ->
+            val info = Camera.CameraInfo()
+            Camera.getCameraInfo(index, info)
+
+            when (info.facing) {
+                Camera.CameraInfo.CAMERA_FACING_FRONT -> "Front Facing"
+                Camera.CameraInfo.CAMERA_FACING_BACK -> "Rear Facing"
+                else -> "Camera ID: $index"
+            }
+        }
+
+        var checkedIndex = 0
+        for (index in 0 until numberOfCameras) {
+            if (index == cameraId) {
+                checkedIndex = index
+            }
+        }
+
+        return AlertDialog.Builder(requireActivity())
+            .setTitle(R.string.select_camera)
+            .setSingleChoiceItems(cameraNames, checkedIndex) { _, which ->
+                cameraId = which
+            }
+            .setPositiveButton(R.string.ok_button) { _, _ ->
+                safeListener.onCameraSelected(cameraId)
+            }
+            .setNegativeButton(R.string.cancel_button) { _, _ ->
+            }
+            .create()
+    }
+
+    companion object {
+        fun newInstance(
+            listener: CameraSelectorDialogListener,
+            cameraId: Int
+        ): CameraSelectorDialogFragment {
+            return CameraSelectorDialogFragment().apply {
+                this.cameraId = cameraId
+                this.listener = listener
+            }
+        }
+    }
+}
