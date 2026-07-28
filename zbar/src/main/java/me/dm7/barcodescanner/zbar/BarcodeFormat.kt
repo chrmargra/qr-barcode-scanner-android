c3d9bf0a24229d44bf3c9b537ce48088e6e12909
Christian <123153172+chrmargra@users.noreply.github.com>
2026-07-28 21:58:29 +0200

    Convert BarcodeFormat to Kotlin

--- zbar/src/main/java/me/dm7/barcodescanner/zbar/BarcodeFormat.kt
+++ zbar/src/main/java/me/dm7/barcodescanner/zbar/BarcodeFormat.kt
@@ -1,73 +1,94 @@
-package me.dm7.barcodescanner.zbar;
+package me.dm7.barcodescanner.zbar
 
-import net.sourceforge.zbar.Symbol;
+import net.sourceforge.zbar.Symbol
 
-import java.util.List;
-import java.util.ArrayList;
+open class BarcodeFormat(
+    open val id: Int,
+    open val name: String?
+) {
 
-public class BarcodeFormat {
+    companion object {
 
-    private final int id;
-    private final String name;
+        @JvmField
+        val NONE = BarcodeFormat(Symbol.NONE, "NONE")
 
-    public static final BarcodeFormat NONE = new BarcodeFormat(Symbol.NONE, "NONE");
-    public static final BarcodeFormat PARTIAL = new BarcodeFormat(Symbol.PARTIAL, "PARTIAL");
-    public static final BarcodeFormat EAN8 = new BarcodeFormat(Symbol.EAN8, "EAN8");
-    public static final BarcodeFormat UPCE = new BarcodeFormat(Symbol.UPCE, "UPCE");
-    public static final BarcodeFormat ISBN10 = new BarcodeFormat(Symbol.ISBN10, "ISBN10");
-    public static final BarcodeFormat UPCA = new BarcodeFormat(Symbol.UPCA, "UPCA");
-    public static final BarcodeFormat EAN13 = new BarcodeFormat(Symbol.EAN13, "EAN13");
-    public static final BarcodeFormat ISBN13 = new BarcodeFormat(Symbol.ISBN13, "ISBN13");
-    public static final BarcodeFormat I25 = new BarcodeFormat(Symbol.I25, "I25");
-    public static final BarcodeFormat DATABAR = new BarcodeFormat(Symbol.DATABAR, "DATABAR");
-    public static final BarcodeFormat DATABAR_EXP = new BarcodeFormat(Symbol.DATABAR_EXP, "DATABAR_EXP");
-    public static final BarcodeFormat CODABAR = new BarcodeFormat(Symbol.CODABAR, "CODABAR");
-    public static final BarcodeFormat CODE39 = new BarcodeFormat(Symbol.CODE39, "CODE39");
-    public static final BarcodeFormat PDF417 = new BarcodeFormat(Symbol.PDF417, "PDF417");
-    public static final BarcodeFormat QRCODE = new BarcodeFormat(Symbol.QRCODE, "QRCODE");
-    public static final BarcodeFormat CODE93 = new BarcodeFormat(Symbol.CODE93, "CODE93");
-    public static final BarcodeFormat CODE128 = new BarcodeFormat(Symbol.CODE128, "CODE128");
+        @JvmField
+        val PARTIAL = BarcodeFormat(Symbol.PARTIAL, "PARTIAL")
 
-    public static final List<BarcodeFormat> ALL_FORMATS = new ArrayList<>();
+        @JvmField
+        val EAN8 = BarcodeFormat(Symbol.EAN8, "EAN8")
 
-    static {
-        ALL_FORMATS.add(BarcodeFormat.PARTIAL);
-        ALL_FORMATS.add(BarcodeFormat.EAN8);
-        ALL_FORMATS.add(BarcodeFormat.UPCE);
-        ALL_FORMATS.add(BarcodeFormat.ISBN10);
-        ALL_FORMATS.add(BarcodeFormat.UPCA);
-        ALL_FORMATS.add(BarcodeFormat.EAN13);
-        ALL_FORMATS.add(BarcodeFormat.ISBN13);
-        ALL_FORMATS.add(BarcodeFormat.I25);
-        ALL_FORMATS.add(BarcodeFormat.DATABAR);
-        ALL_FORMATS.add(BarcodeFormat.DATABAR_EXP);
-        ALL_FORMATS.add(BarcodeFormat.CODABAR);
-        ALL_FORMATS.add(BarcodeFormat.CODE39);
-        ALL_FORMATS.add(BarcodeFormat.PDF417);
-        ALL_FORMATS.add(BarcodeFormat.QRCODE);
-        ALL_FORMATS.add(BarcodeFormat.CODE93);
-        ALL_FORMATS.add(BarcodeFormat.CODE128);
-    }
+        @JvmField
+        val UPCE = BarcodeFormat(Symbol.UPCE, "UPCE")
 
-    public BarcodeFormat(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
+        @JvmField
+        val ISBN10 = BarcodeFormat(Symbol.ISBN10, "ISBN10")
 
-    public int getId() {
-        return id;
-    }
+        @JvmField
+        val UPCA = BarcodeFormat(Symbol.UPCA, "UPCA")
 
-    public String getName() {
-        return name;
-    }
+        @JvmField
+        val EAN13 = BarcodeFormat(Symbol.EAN13, "EAN13")
 
-    public static BarcodeFormat getFormatById(int id) {
-        for (BarcodeFormat format : ALL_FORMATS) {
-            if (format.getId() == id) {
-                return format;
+        @JvmField
+        val ISBN13 = BarcodeFormat(Symbol.ISBN13, "ISBN13")
+
+        @JvmField
+        val I25 = BarcodeFormat(Symbol.I25, "I25")
+
+        @JvmField
+        val DATABAR = BarcodeFormat(Symbol.DATABAR, "DATABAR")
+
+        @JvmField
+        val DATABAR_EXP = BarcodeFormat(Symbol.DATABAR_EXP, "DATABAR_EXP")
+
+        @JvmField
+        val CODABAR = BarcodeFormat(Symbol.CODABAR, "CODABAR")
+
+        @JvmField
+        val CODE39 = BarcodeFormat(Symbol.CODE39, "CODE39")
+
+        @JvmField
+        val PDF417 = BarcodeFormat(Symbol.PDF417, "PDF417")
+
+        @JvmField
+        val QRCODE = BarcodeFormat(Symbol.QRCODE, "QRCODE")
+
+        @JvmField
+        val CODE93 = BarcodeFormat(Symbol.CODE93, "CODE93")
+
+        @JvmField
+        val CODE128 = BarcodeFormat(Symbol.CODE128, "CODE128")
+
+        @JvmField
+        val ALL_FORMATS: MutableList<BarcodeFormat> = arrayListOf(
+            PARTIAL,
+            EAN8,
+            UPCE,
+            ISBN10,
+            UPCA,
+            EAN13,
+            ISBN13,
+            I25,
+            DATABAR,
+            DATABAR_EXP,
+            CODABAR,
+            CODE39,
+            PDF417,
+            QRCODE,
+            CODE93,
+            CODE128
+        )
+
+        @JvmStatic
+        fun getFormatById(id: Int): BarcodeFormat {
+            for (format in ALL_FORMATS) {
+                if (format.id == id) {
+                    return format
+                }
             }
+
+            return NONE
         }
-        return BarcodeFormat.NONE;
     }
 }
